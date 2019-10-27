17681b155b52dc56886d9fef899ebd9157b18ddd
GermanAndresMartinez1801 <56598671+GermanAndresMartinez1801@users.noreply.github.com>
2019-10-27 12:41:08 -0500

    Add files via upload

---
+++ 03_personal-info.py
@@ -0,0 +1,7 @@
+print("UNIVERSIDAD POLITECNICA SALESIANA " )
+Nom1=input(" Cual es su primer Nombre ?" )
+Nom2=input(" Cual es su primer Apeliido ?" )
+Ubi=input(" Donde se encuentra ?" )
+Edad= input("Cual es su edad ?" )
+Espacio=" "
+print("Sus nombres son "+ Nom1 + Espacio +Nom2 + " Se encuentra en "+Ubi +" Y su edad es:"+ Edad)
---
+++ 04_if-vian.py
@@ -0,0 +1,6 @@
+Val1=1
+Val2=100
+if Val1==Val2:
+    print(" LOS VALORES SON IGUALES")
+else:
+    print("LOS VALORES SON DIFERENTES")
